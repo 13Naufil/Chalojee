48a2f19d13ee0de944096f8a731d0ef353575b07
Naufil khan <naufil_khan13@hotmail.com>
2018-04-02 17:06:43 +0500

    initial commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,11 +25,10 @@ class Welcome extends CI_Controller {
 	public function DestinationCities(){
         $countries = $this->common->CountryList()[0];
 
-
         if(!empty($countries['CountryListResponse']))
         {
             $arr = array();
-            $arr2 = array();
+            
             if($countries['CountryListResponse']['Status']['StatusCode'] == 1){
                 foreach ($countries['CountryListResponse']['CountryList']['Country'] as $key => $data)
                 {
@@ -38,17 +37,17 @@ class Welcome extends CI_Controller {
                     if(!empty($cityList['DestinationCityListResponse'])) {
                         if ($cityList['DestinationCityListResponse']['Status']['StatusCode'] == 1) {
                             foreach ($cityList['DestinationCityListResponse']['CityList']['City'] as $key2 => $cities) {
-                                $arr[] = array('CityCode' => isset($cities['@attributes']['CityCode']) ? $cities['@attributes']['CityCode'] : 0, 'CityName' => $cities['@attributes']['CityName'] . " , " . $cityList['DestinationCityListResponse']['CountryName']);
-                                //$data = array('CityCode'=> (isset($cities['@attributes']['CityCode']) != null ? $cities['@attributes']['CityCode'] : 0),'CityName'=>$cities['@attributes']['CityName']." , ".$cityList['DestinationCityListResponse']['CountryName']);
 
-                                //$this->search->add($data);
+                                if(isset($cities['@attributes']['CityCode']) && isset($cities['@attributes']['CityName']) && isset($cityList['DestinationCityListResponse']['CountryName'])){
+                                    $arr[] = array('CityCode' => $cities['@attributes']['CityCode'], 'CityName' => $cities['@attributes']['CityName']. " , " . $cityList['DestinationCityListResponse']['CountryName']);
+                                }
                             }
                         }
 
                     }
 
-                }print_r($arr);exit;
-               // return $arr;
+                }
+                return $arr;
             }
         }
     }
